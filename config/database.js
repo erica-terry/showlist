72639d09c5b805b6ac4b8a31bf78041a29fe5c4b
Erica-Terry <eterry@knights.ucf.edu>
2015-11-17 12:10:21 -0500

    changed login credentials to be correct

--- config/database.js
+++ config/database.js
@@ -1,6 +1,6 @@
 module.exports = {
     
-	'url' : 'mongodb://eterry:qyElDhnSPr4U@ds055574.mongolab.com:55574/showlist'
+	'url' : 'mongodb://eterry:900twentytwo@ds055574.mongolab.com:55574/showlist'
 
     // (For c9) 'url' :  'mongodb://localhost:27017/showlist',
 }
